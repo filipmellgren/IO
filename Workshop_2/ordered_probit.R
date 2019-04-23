89798cd2781ac9cd74113b99da5ea9f626c7661a
filipmellgren <filip.mellgren@gmail.com>
2019-04-23 16:56:44 +0200

    Went through all question up to Q7

--- Workshop_2/ordered_probit.R
+++ Workshop_2/ordered_probit.R
@@ -79,7 +79,7 @@ profit = function(a1, a2, a3, a4, a5, g1, g2, g3, g4, g5, gL, gL2,
 }
 
 
-ML_full <- mle(profit, start = list(a1 = -3.73, a2 = 0.72, a3 = .127, a4 = 0.424, a5 = 0.128,
+ML_2 <- mle(profit, start = list(a1 = -3.73, a2 = 0.72, a3 = .127, a4 = 0.424, a5 = 0.128,
                                     g1 = 9.1, g2 = 1.04, g3 = 0.979, g4 = 0.047, g5 = 0.29, 
                                     gL = 0.43, gL2 = -3.9, 
                                     beta1 = -1.02, beta2 = 3.66, 
@@ -87,11 +87,51 @@ ML_full <- mle(profit, start = list(a1 = -3.73, a2 = 0.72, a3 = .127, a4 = 0.424
                                     beta5 = 0.1, beta6 = 0.1,
                                     lambda1 = -1.8, lambda2 = 3.96, lambda3 = 1))
 
-summary(ML_full)
+summary(ML_2)
 
 
 
 table <- xtable(cbind(as.matrix(ML_full@coef), as.matrix(sqrt(diag(ML_full@vcov)))))
 
 
+## THIS IS THE ONE:
+# Y: pop, pos_gpop, neg_gpop 
+# X (local market characteristics): s_kids, s_pens, s_young, s_women, consumption per capita, wage
+# W (fixed costs): hprice, wage
+
+df <- df %>% drop_na()
+
+profit = function(a1, a2, a3, a4, a5, g1, g2, g3, g4, g5, gL, gL2,
+                  beta1, beta2, beta3, beta4, beta5, beta6,
+                  lambda1, lambda2){
+  
+  df <- df %>% mutate(
+    FC = g1 + gL*hprice + gL2*wage, # Monopolist's fixed cost
+    V = a1+ beta1*s_kids + beta2*s_young + beta3*s_pens+ beta4*s_women + beta5*cons_pc + beta6*wage,
+    S = pop + pos_gpop * lambda1 + neg_gpop * lambda2,
+    P1 = pnorm(S * V - FC), # Monopolist's profit
+    P2 = pnorm(S * (V  - a2) - FC  - g2), # duopolists' profit
+    P3 = pnorm(S * (V  - a2 - a3) - FC  - g2 - g3), # etc.
+    P4 = pnorm(S * (V  - a2 - a3 - a4) - FC - g2 - g3 - g4),
+    P5 = pnorm(S * (V  - a2 - a3 - a4 - a5) - FC - g2 - g3 - g4 - g5)
+  )
+  
+  df <- df %>% mutate(lnf = case_when(
+    stores == 0 ~ log(1 - P1),
+    stores == 1 ~ log(P1 -P2),
+    stores == 2 ~ log(P2 -P3),
+    stores == 3 ~ log(P3 -P4),
+    stores == 4 ~ log(P4 -P5),
+    stores == 5 ~ log(P5)))
+  
+  # return the negative to maximize rather than minimize
+  -sum(df$lnf)
+}
+
+
+ML_23 <- mle(profit, start = list(a1 = -3.73, a2 = 0.72, a3 = .127, a4 = 0.424, a5 = 0.128,
+                                    g1 = 9.1, g2 = 1.04, g3 = 0.979, g4 = 0.047, g5 = 0.29, 
+                                    gL = 0.43, gL2 = -3.9, 
+                                    beta1 = 0, beta2 = -1.02, beta3 = -3.6, beta4 = 8.9,
+                                    beta5 = 0, beta6 = 0, lambda1 = -1.8, lambda2 = 3.96))
 
