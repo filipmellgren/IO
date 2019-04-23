8119cb81da0c81181967fc043ffa8a10e3ea7d71
filipmellgren <filip.mellgren@gmail.com>
2019-04-23 18:35:52 +0200

    fixed a table

--- Workshop_2/ordered_probit.R
+++ Workshop_2/ordered_probit.R
@@ -91,23 +91,22 @@ summary(ML_2)
 
 
 
-table <- xtable(cbind(as.matrix(ML_full@coef), as.matrix(sqrt(diag(ML_full@vcov)))))
 
 
 ## THIS IS THE ONE:
 # Y: pop, pos_gpop, neg_gpop 
-# X (local market characteristics): s_kids, s_pens, s_young, s_women, consumption per capita, wage
+# X (local market characteristics): s_kids, s_pens, s_young, s_women, consumption per capita,
 # W (fixed costs): hprice, wage
 
 df <- df %>% drop_na()
 
 profit = function(a1, a2, a3, a4, a5, g1, g2, g3, g4, g5, gL, gL2,
-                  beta1, beta2, beta3, beta4, beta5, beta6,
+                  beta1, beta2, beta3, beta4, beta5,
                   lambda1, lambda2){
   
   df <- df %>% mutate(
     FC = g1 + gL*hprice + gL2*wage, # Monopolist's fixed cost
-    V = a1+ beta1*s_kids + beta2*s_young + beta3*s_pens+ beta4*s_women + beta5*cons_pc + beta6*wage,
+    V = a1+ beta1*s_kids + beta2*s_young + beta3*s_pens+ beta4*s_women + beta5*cons_pc ,
     S = pop + pos_gpop * lambda1 + neg_gpop * lambda2,
     P1 = pnorm(S * V - FC), # Monopolist's profit
     P2 = pnorm(S * (V  - a2) - FC  - g2), # duopolists' profit
@@ -133,5 +132,19 @@ ML_23 <- mle(profit, start = list(a1 = -3.73, a2 = 0.72, a3 = .127, a4 = 0.424, 
                                     g1 = 9.1, g2 = 1.04, g3 = 0.979, g4 = 0.047, g5 = 0.29, 
                                     gL = 0.43, gL2 = -3.9, 
                                     beta1 = 0, beta2 = -1.02, beta3 = -3.6, beta4 = 8.9,
-                                    beta5 = 0, beta6 = 0, lambda1 = -1.8, lambda2 = 3.96))
+                                    beta5 = 0, lambda1 = -1.8, lambda2 = 3.96))
+
+summary(ML_23)
+table <- cbind(as.matrix(ML_23@coef), as.matrix(sqrt(diag(ML_23@vcov))))
+table <- table %>% as_tibble(rownames = "variable") %>% 
+  mutate(z = V1 / V2, p = (1-pnorm(z)),
+         variable = c("a1", "a2", "a3", "a4", "a5", 
+                      "g1", "g2", "g3", "g4", "g5", 
+                      "House prices", "Wages","Fraction kids", "Fraction young", 
+                      "Fraction pensioner","Fraction Women", "Consumption/capita", 
+                      "Positive growth", "Negative growth")) %>%
+  rename(Coef. = V1, s.e. = V2)
+
+xtable::xtable(table, caption = "ML estimates")
+
 
