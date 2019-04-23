1c9713d7431174e955c396ccca1b36a61ae9ec8f
filipmellgren <filip.mellgren@gmail.com>
2019-04-23 08:48:07 +0200

    Some changes in the oprdered probit. Fit a specification

--- Workshop_2/ordered_probit.R
+++ Workshop_2/ordered_probit.R
@@ -14,7 +14,6 @@ df <- df %>% drop_na()
 profit = function(a1, a2, a3, a4, a5, g1, g2, g3, g4, g5, gL, gL2,
                   beta1, beta2, beta3, beta4, lambda1, lambda2){
   
-  
   df <- df %>% mutate(
     FC = g1 + gL*hprice + gL2*wage, # Monopolist's fixed cost
     V = a1+ beta1*s_young + beta2*s_pens+ beta3*s_women + beta4*consumption ,
@@ -46,15 +45,20 @@ ML_full <- mle(profit, start = list(a1 = -3.73, a2 = 0.72, a3 = .127, a4 = 0.424
                          lambda1 = -1.8, lambda2 = 3.96))
 
 
-### DROP some variables:
-profit2 = function(a1, a2, a3, a4, a5, g1, g2, g3, g4, g5, gL, gL2,
-                  beta4, lambda1){
-  
+#### Change specification:
+#Y: pop, pos_gpop, neg_gpop, close_pop
+# X s_kids, s_pens, s_young, s_women, consumption, cons_pc
+# W (fixed costs): hprice wage
+
+profit = function(a1, a2, a3, a4, a5, g1, g2, g3, g4, g5, gL, gL2,
+                  beta1, beta2, beta3, beta4, beta5, beta6, 
+                  lambda1, lambda2, lambda3){
   
   df <- df %>% mutate(
     FC = g1 + gL*hprice + gL2*wage, # Monopolist's fixed cost
-    V = a1+ beta4*consumption ,
-    S = pop + gpop * lambda1,
+    V = a1+ beta1*s_young + beta2*s_pens+ beta3*s_women + beta4*consumption +
+      beta5*s_kids + beta6*cons_pc,
+    S = pop + pos_gpop * lambda1 + neg_gpop * lambda2 + close_pop * lambda3,
     P1 = pnorm(S * V - FC), # Monopolist's profit
     P2 = pnorm(S * (V  - a2) - FC  - g2), # duopolists' profit
     P3 = pnorm(S * (V  - a2 - a3) - FC  - g2 - g3), # etc.
@@ -75,10 +79,19 @@ profit2 = function(a1, a2, a3, a4, a5, g1, g2, g3, g4, g5, gL, gL2,
 }
 
 
+ML_full <- mle(profit, start = list(a1 = -3.73, a2 = 0.72, a3 = .127, a4 = 0.424, a5 = 0.128,
+                                    g1 = 9.1, g2 = 1.04, g3 = 0.979, g4 = 0.047, g5 = 0.29, 
+                                    gL = 0.43, gL2 = -3.9, 
+                                    beta1 = -1.02, beta2 = 3.66, 
+                                    beta3 = 8.9, beta4 = 0.01, 
+                                    beta5 = 0.1, beta6 = 0.1,
+                                    lambda1 = -1.8, lambda2 = 3.96, lambda3 = 1))
 
-ML2 <- mle(profit2, start = list(a1 = 1.3, a2 = 0.01, a3 = 0.05, a4 = 0.5, a5 = 0.1,
-                               g1 = 5.9, g2 = 1, g3 = 1, g4 = 0, g5 = 0.2, 
-                               gL = 0.3, gL2 = -2.5, 
-                               beta4 = 0.01,
-                               lambda1 = -1))
+summary(ML_full)
+
+
+
+table <- xtable(cbind(as.matrix(ML_full@coef), as.matrix(sqrt(diag(ML_full@vcov)))))
+
+
 
