16c860ef16cced901dc1a1b2dd1861d8160aad39
filipmellgren <filip.mellgren@gmail.com>
2019-05-08 19:15:49 +0200

    Fixed the table and updated the theta estimates

--- Workshop_4/calculating_theta.R
+++ Workshop_4/calculating_theta.R
@@ -2,14 +2,14 @@
 stargazer(qd, ps.po, ps.pn, iv.qd.po, iv.qd.pn, iv.ps.po, iv.ps.pn, type = "text")
 
 # From IV using PO
-beta2 <- 0.365 # Coefficient before PO/PN. beta3 in Porter
-alpha1 <- -0.751 # Coefficient before log(gr)
-theta <- alpha1/(exp(beta3)) -alpha1
+beta2 <- 0.368 # Coefficient before PO/PN. beta3 in Porter
+alpha1 <- -0.703 # Coefficient before log(gr)
+theta <- alpha1/(exp(beta2)) -alpha1
 
 # From IV using PN
-beta2 <-  0.416 # Coefficient before PO/PN. beta3 in Porter
-alpha1 <- -0.922 # Coefficient before log(gr)
-theta <- alpha1/(exp(beta3)) -alpha1
+beta2 <-  0.418 # Coefficient before PO/PN. beta3 in Porter
+alpha1 <- -0.703 # Coefficient before log(gr)
+theta <- alpha1/(exp(beta2)) -alpha1
 
 
 
