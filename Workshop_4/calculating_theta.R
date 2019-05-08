430b84e0534639f108b84387fdc993d52a7cf2ed
filipmellgren <filip.mellgren@gmail.com>
2019-05-08 11:26:30 +0200

    Added calculations using PN

--- Workshop_4/calculating_theta.R
+++ Workshop_4/calculating_theta.R
@@ -1,7 +1,15 @@
-stargazer(qs, qd,iv_qs, iv_qd, type = "text")
+# This file calculates two estimates of the market conduct paramter theta.
+stargazer(qd, ps.po, ps.pn, iv.qd.po, iv.qd.pn, iv.ps.po, iv.ps.pn, type = "text")
 
 # From IV using PO
-beta2 <- 0.365 # Coefficient before PO. beta3 in Porter
+beta2 <- 0.365 # Coefficient before PO/PN. beta3 in Porter
 alpha1 <- -0.751 # Coefficient before log(gr)
 theta <- alpha1/(exp(beta3)) -alpha1
+
+# From IV using PN
+beta2 <-  0.416 # Coefficient before PO/PN. beta3 in Porter
+alpha1 <- -0.922 # Coefficient before log(gr)
+theta <- alpha1/(exp(beta3)) -alpha1
+
+
 
